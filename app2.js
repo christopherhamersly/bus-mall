d73609118412658de59ef2ec20e2a8aa7ebbf822
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-05-13 08:10:39 -0700

    chart added

--- app2.js
+++ app2.js
@@ -133,7 +133,7 @@ function generateChart(){
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)'
         ],
-        borderWidth: 1
+        borderWidth: 3
       }]
     },
     options: {
